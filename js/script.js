3ff4b1b133fa994628a7accca0329be8afaa396c
Satskhi <w.wesley.af@gmail.com>
2016-12-25 16:43:19 -0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -219,10 +219,9 @@ function autowoot() {
         $('.omega-theme-toggle-autowoot').children('.omega-menu-icon').show();
         omegaTheme.autowoot = true;
         console.info('[Omega] Enabled autowoot.');
-		API.on(API.ADVANCE, callback(obj){ 
-			setTimeout(function(){
+		API.on(API.DJ_ADVANCE, callback);
+			setTimeout(function callback(obj) { 
 				$('#woot').click();
-			}, 1000);
-		});	
+			}, 1000);	
     }
 }
